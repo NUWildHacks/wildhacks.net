dc78e203213af5bd4521882c7cda3dc82b14721f
Emily Zhang <emily.fangdi.zhang@hotmail.com>
2023-01-25 17:29:20 -0600

    header complete

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+    return (
+    <div className="header">
+        <ul>
+            <li><a href="default.asp">home</a></li>
+            <li><a href="news.asp">about</a></li>
+            <li><a href="contact.asp">tracks</a></li>
+            <li><a href="about.asp">sponsors</a></li>
+            <li><a href="about.asp">FAQ</a></li>
+        </ul>
+        
+    </div>
+    );
+  }
