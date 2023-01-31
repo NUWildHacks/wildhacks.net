2c4b3038a068add64750910c9e1f628291431486
Dilan <dilan@dilanxd.com>
2023-01-31 15:28:06 -0600

    fix

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -30,7 +30,7 @@ function Heading() {
     <Container>
       <Time>April 15-16 2023</Time>
       <Title>2023 WildHacks registration is now open!</Title>
-      <Button href="https://wildhacks.net">Register</Button>
+      <Button href="#">Register</Button>
     </Container>
   );
 }
