f3bd4777308b6cd0bfae52e24075d93a45d0fa49
Dilan <dilan@dilanxd.com>
2023-03-04 00:11:39 -0600

    add new sponsors

--- src/Sponsors.jsx
+++ src/Sponsors.jsx
@@ -18,6 +18,7 @@ const Content = styled.div`
   gap: 32px;
   flex-wrap: wrap;
   background-color: #ffffff;
+  box-shadow: 0 0 16px #ffffff;
   padding: 16px;
   border-radius: 12px;
 `;
@@ -25,13 +26,29 @@ const Content = styled.div`
 function Sponsors() {
   return (
     <Container>
-      <Title>— SPONSORED BY —</Title>
+      <Title>— OUR SPONSORS —</Title>
       <Content>
         <Sponsor src="/imc.png" alt="IMC" href="https://www.imc.com/us/" />
         <Sponsor
           src="/northwestern-cs.png"
           alt="Northwestern Computer Science"
           href="https://www.mccormick.northwestern.edu/computer-science/"
+        />
+        <Sponsor
+          src="/deloitte.png"
+          alt="Deloitte"
+          href="https://www2.deloitte.com/us/en.html"
+        />
+        <Sponsor
+          src="/spiderrock.png"
+          alt="SpiderRock"
+          href="https://www.spiderrock.net/"
+        />
+        <Sponsor
+          src="/thegarage.jpeg"
+          alt="The Garage"
+          href="https://thegarage.northwestern.edu/"
+          h={128}
         />
       </Content>
     </Container>
--- src/components/Sponsor.jsx
+++ src/components/Sponsor.jsx
@@ -5,22 +5,23 @@ const Container = styled.a`
   display: block;
 
   img {
-    height: 64px;
+    height: ${(props) => props.h || 64}px;
     transition: transform 0.2s ease-in-out;
     ${mobile} {
       max-width: 100%;
       height: auto;
+      height: ${(props) => props.h || 64}px;
     }
   }
 
   &:hover img {
-    transform: scale(1.05);
+    transform: scale(1.02);
   }
 `;
 
-export default function Sponsor({ href, src, alt }) {
+export default function Sponsor({ href, src, alt, h }) {
   return (
-    <Container href={href} target="_blank" rel="noreferrer">
+    <Container href={href} target="_blank" rel="noreferrer" h={h}>
       <img src={src} alt={alt} />
     </Container>
   );
